b07f1790c85b05cd538332e2c1de08dd76e38749
jshlbrd <liburdi.joshua@gmail.com>
2016-01-30 09:58:12 -0500

    bumped copyright

--- meta_pdf/meta_pdf.py
+++ meta_pdf/meta_pdf.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Josh Liburdi
+# Copyright 2016 Josh Liburdi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
