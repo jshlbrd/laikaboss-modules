a263cc62be07f20649ae8e86f6f718e140b29938
jshlbrd <liburdi.joshua@gmail.com>
2016-01-30 09:16:35 -0500

    init explode_pdf

---
+++ explode_pdf/README.md
@@ -0,0 +1,45 @@
+explode_pdf.py
+================
+
+This module explodes PDF files and extracts any embedded files, Flash objects, and JavaScript code within the file. It uses peepdf for PDF parsing and object extraction.
+
+Note that extraction can add several seconds to minutes of parsing time for PDF files that contain many object streams. Users have the option to specify force and loose mode for peepdf parsing in the dispatch.yara file. Peepdf can be accessed here: https://github.com/jesparza/peepdf
+
+Sample output
+```
+ "objectSize": 10402,
+      "fileType": [
+        "officex"
+      ],
+      "level": 2,
+      "filename": "e_pdf_stream_1430",
+      "depth": 1,
+      "sourceModule": "EXPLODE_PDF",
+      "flags": [],
+```
+
+Installation
+---
+* Mkdir pypackages in directory laikaboss/laikaboss/modules
+* Create empty __init__.py file in directory laikaboss/laikaboss/modules/pypackages
+* Git clone peepdf in directory laikaboss/laikaboss/modules/pypackages
+* Create empty __init__.py file in directory laikaboss/laikaboss/modules/pypackages/peepdf
+* Put explode_pdf.py in directory laikaboss/laikaboss/modules
+* Modify dispatch.yara to include the module
+```
+rule type_is_pdf
+{
+    meta:
+	scan_modules = "EXPLODE_PDF(loose=True,force=True)"
+	file_type = "pdf"
+    strings:
+        $pdf1 = { 25 50 44 46 2d ?? 2e } // %PDF-.(dot)
+        $pdf2 = { 25 50 44 46 2d }
+    condition:
+
+        ($pdf1 in (0 .. 1024) or $pdf2 at 0) and not (type_is_zip or
+                                                      type_is_msoffice2007 or
+                                                      type_is_tar or
+                                                      type_is_rar)
+}
+```
---
+++ explode_pdf/explode_pdf.py
@@ -0,0 +1,78 @@
+# Copyright 2015 Josh Liburdi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from laikaboss.si_module import SI_MODULE
+from laikaboss.objectmodel import ModuleObject, ExternalVars
+from laikaboss import config
+import tempfile
+import os
+from .pypackages.peepdf.PDFCore import PDFParser
+
+class EXPLODE_PDF(SI_MODULE):
+    def __init__(self,):
+        self.module_name = "EXPLODE_PDF"
+        self.TEMP_DIR = '/tmp/laikaboss_tmp'
+        if hasattr(config, 'tempdir'):
+                self.TEMP_DIR = config.tempdir.rstrip('/')
+        if not os.path.isdir(self.TEMP_DIR):
+                os.mkdir(self.TEMP_DIR)
+                os.chmod(self.TEMP_DIR, 0777)
+
+    def _run(self, scanObject, result, depth, args):
+        moduleResult = []
+        metaDict = {}
+
+        if 'force' in args:
+                force_mode = bool(args['force'])
+        else:
+                force_mode = True
+
+        if 'loose' in args:
+                loose_mode = bool(args['loose'])
+        else:
+                loose_mode = True
+
+        with tempfile.NamedTemporaryFile(dir=self.TEMP_DIR) as temp_file:
+                temp_file_name = temp_file.name
+                temp_file.write(scanObject.buffer)
+                temp_file.flush()
+
+                pdfparser = PDFParser()
+                ret,pdf = pdfparser.parse(temp_file_name, force_mode, loose_mode)
+
+                # List of objects to explode.
+                explode_objects = []
+
+                for versions in pdf.getStats()['Versions']:
+                        if 'Objects with JS code' in versions and versions['Objects with JS code'] != None:
+                                # The list of JS code object references is always the second value in this list.
+                                explode_objects += versions['Objects with JS code'][1]
+                        if 'Elements' in versions and type(versions['Elements']) == dict:
+                                        # The list of Elements contains multiple object references.
+                                        elements_list = ['/EmbeddedFile','/EmbeddedFiles','/Flash']
+                                        for key,val in versions['Elements'].iteritems():
+                                                if key in elements_list:
+                                                        explode_objects += val
+
+                for object_id in explode_objects:
+                        name = 'stream_' + str(object_id)
+                        pdf_object = pdf.getObject(object_id,None)
+                        if pdf_object.getType() == 'stream':
+                                try:
+                                        moduleResult.append(ModuleObject(buffer=pdf_object.getStream(), externalVars=ExternalVars(filename='e_pdf_%s' % name)))
+                                except:
+                                        scanObject.addFlag('explode_pdf:err:explode_%s_failed' % name)
+                                        pass
+
+        return moduleResult
